4182966efb0f6bd0a2454a5f909e67330e5241ec
443_President <gonzares443@gmail.com>
2020-07-23 22:57:43 +0900

    ギットギト会に向けての練習ページにすることにしました

---
+++ js/common.js
@@ -0,0 +1,17 @@
+console.log("ハロワやっふい");
+console.log("羊が"+2+2+"匹");
+console.log(114+514);
+
+var number = 6;
+console.log(number);
+
+number = 114514;
+console.log(number);
+
+console.log(3+2);
+console.log(3*2);
+console.log(6/3);
+console.log(5%3);
+
+var a = 1;
+console.log(a+=1);
